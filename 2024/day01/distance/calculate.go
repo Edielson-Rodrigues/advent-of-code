45d99d225f01ddf68f9589f61bef6fd015ae35a2
Edielson-Rodrigues <edielsonrodrigues567@gmail.com>
2025-08-31 01:40:49 -0300

    refactor: move function calculateTotalDistance to day01/distance

---
+++ 2024/day01/distance/calculate.go
@@ -0,0 +1,26 @@
+package distances
+
+import (
+	"advent-of-code/pkg/lists"
+	"advent-of-code/pkg/numbers"
+	"advent-of-code/pkg/sorting"
+)
+
+func Calculate(leftList []int, rightList []int) int {
+	sorting.MergeSort(leftList)
+	sorting.MergeSort(rightList)
+
+	biggerListSize := numbers.GetBiggest(len(leftList), len(rightList))
+
+	totalDistance := 0
+
+	for i := range biggerListSize {
+		leftNumber := lists.GetSafe(leftList, i)
+		rightNumber := lists.GetSafe(rightList, i)
+
+		distance := numbers.GetBiggest(leftNumber, rightNumber) - numbers.GetSmallest(leftNumber, rightNumber)
+		totalDistance += distance
+	}
+
+	return totalDistance
+}
